bae66c032ae563c6503ca580fcc635d603060af3
Andrei Bonteanu <andrei.bonteanu@utexas.edu>
2016-03-06 02:04:42 -0600

    MakeLadder
    outer decomposed method of make ladder, accounted for dead end words
    and repeat words in solution list

--- Starter_Code_export/WordLadderSolver.java
+++ Starter_Code_export/WordLadderSolver.java
@@ -66,6 +66,50 @@ public class WordLadderSolver implements Assignment4Interface {
         }
     	return true;
     }
+    
+    /**
+     * Make a ladder recursively checking every possible path from each node. This is
+     * an exhaustive search where if we reach a dead end, the word is remembered such
+     * that no future node will pass through it again. For each next word in the 
+     * recursion, a list of candidates is generated of which each word is only one
+     * letter away from the current word without changing the letter that was just
+     * previously altered (to progress). Then, each candidate word is checked to
+     * see how far away it is from the destination word. The word closest is picked
+     * first, then the others follow. Recursion ends when we have looked through all
+     * possible paths or found a possible path.
+     * 
+     * @param fromWord: First word of ladder
+     * @param toWord: Last word of ladder
+     * @param posChanged: Last character position changed
+     * @return true/false: If ladder was successfully generated or not
+     */
+    private boolean makeLadder(String fromWord, String toWord, int posChanged) {
+    	solutionList.add(fromWord); //automatically add word if we pass through it
+    	if (fromWord.equals(toWord)) return true; //done if going from word to same word
+    	String endLadder = checkIfOneAway(fromWord, toWord); //done if word is one away from endWord
+    	ArrayList<String> candidates = Assign4Driver.dictionary.matchAllButOne(fromWord, posChanged);
+    	candidates = removeDuplicates(candidates); //remove candidate words already in ladder
+    	candidates = removeDeadWords(candidates); //remove dead end words
+    	if (endLadder != null) { 
+    		solutionList.add(endLadder); //add last word and terminate
+    		return true;
+    	}
+    	prependMismatch(candidates, toWord); //add number of differences in letter from endWord
+    	Collections.sort(candidates); //sort by alphabetical order (fewest differences first)
+    	
+    	//go through each possible candidate to search for possible ladder
+    	for (String word : candidates) {
+    		String nextWord = word.substring(1); //ignore the number (letter difference from endWord)
+    		int diffLetter = findPositionChanged(nextWord, fromWord); //find where it differs from previous
+    		boolean done = makeLadder(nextWord, toWord, diffLetter); //recursive call with new word
+    		if (done) return true; //if end found, exit recursion
+    	}
+    	
+    	//case if dead end reached from previous stack frame path
+    	solutionList.remove(fromWord); //remove word from solution list
+    	deadList.add(fromWord); //word is a "dead end"
+		return false; //return false to pop up in recursion stack
+    }
 
     // add additional methods here
 }
