a2b7e7d987961e40a5fe44772c45aa7fa42f2589
Andrei Bonteanu <andrei.bonteanu@utexas.edu>
2016-03-06 00:33:14 -0600

    Exception Class
    comments added and finalized

--- Starter_Code_export/NoSuchLadderException.java
+++ Starter_Code_export/NoSuchLadderException.java
@@ -1,16 +1,31 @@
+/*
+    Names: Andrei Bonteanu, Katelyn Kelsey
+    EIDs: fab542, kmk2669
+    Solution for EE422C assignment4
+    Lab Section: thursday @ 2:00 pm
+*/
+
 package assignment4;
 
-public class NoSuchLadderException extends Exception
-{
-    private static final long serialVersionUID = 1L;
+public class NoSuchLadderException extends Exception {
+    private static final long serialVersionUID = 1L; //identifier
 
-    public NoSuchLadderException(String message)
-    {
+    /**
+     * Constructor with only error message
+     * 
+     * @param message: Error message to display
+     */
+    public NoSuchLadderException(String message) {
         super(message);
     }
 
-    public NoSuchLadderException(String message, Throwable throwable)
-    {
+    /**
+     * Constructor with error message and throwable object
+     * 
+     * @param message: Error to display
+     * @param throwable
+     */
+    public NoSuchLadderException(String message, Throwable throwable) {
         super(message, throwable);
     }
 }
